07d4004680423aee45bca828d5dedfca6f2b8942
YUMGUY <timothytipham@gmail.com>
2022-02-04 17:29:36 -0600

    this is the first test
    - added a comment in a created script
    called testExample1

---
+++ Assets/testing/testExample1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testExample1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this is a test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
