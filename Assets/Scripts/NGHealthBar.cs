b22c348a5f24932fe650213cb7723f4ca6e380bc
YUMGUY <timothytipham@gmail.com>
2022-04-28 14:46:58 -0500

    when hp 0, game over bro

--- Assets/Scripts/NGHealthBar.cs
+++ Assets/Scripts/NGHealthBar.cs
@@ -30,38 +30,50 @@ public class NGHealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hotdps = (100 - overheat.heat) / 5;
-        hp += -Time.deltaTime * Mathf.Sqrt(track.numberofTasks * hotdps);
+
         if (hp <= 0)
         {
-            active.sprite = hb0;
+            print("game over bro");
+            hp = 0;
         }
         else
-            if(hp <= 20)
-            {
-                active.sprite = hb20;
-            }
-            else
-                if(hp <= 40)
-                {
-                    active.sprite = hb40;
-                }
-                else
-                    if(hp <= 60)
+        {
+            hotdps = (100 - overheat.heat) / 5;
+                    hp += -Time.deltaTime * Mathf.Sqrt(track.numberofTasks * hotdps);
+                    if (hp <= 0)
                     {
-                        active.sprite = hb60;
+                        active.sprite = hb0;
                     }
                     else
-                        if(hp <= 80)
+                        if(hp <= 20)
                         {
-                            active.sprite = hb80;
+                            active.sprite = hb20;
                         }
                         else
-                        {
-                            active.sprite = hb100;
-                        }
+                            if(hp <= 40)
+                            {
+                                active.sprite = hb40;
+                            }
+                            else
+                                if(hp <= 60)
+                                {
+                                    active.sprite = hb60;
+                                }
+                                else
+                                    if(hp <= 80)
+                                    {
+                                        active.sprite = hb80;
+                                    }
+                                    else
+                                    {
+                                        active.sprite = hb100;
+                                    }
 
-        num.text = hp.ToString("F2");
+                    num.text = hp.ToString("F2");
+        }
+        
+
+    
     }
 
     void addHealth(float bonus)
